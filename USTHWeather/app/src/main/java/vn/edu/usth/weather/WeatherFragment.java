bd15db5947dcc835ff3f217b37eabc50c7c97bcc
ngknnnn <namkhanh2903@gmail.com>
2019-12-04 14:13:23 +0700

    Lab 10

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +18,6 @@ public class WeatherFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_forecast2, container, false);
+        return inflater.inflate(R.layout.fragment_weather, container, false);
     }
 }
